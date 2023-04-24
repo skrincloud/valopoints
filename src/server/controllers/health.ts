a17fae185657362041b3918903ae89909ebc7b6b
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-04-23 22:26:19 -0500

    VLP-5 Enable global settings from content

--- src/server/controllers/health.ts
+++ src/server/controllers/health.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express'
 import MySQLDatabase from '../vendors/mysql'
 import { Valorant } from '../vendors/valorant'
+import { Content } from '../vendors/content'
 
 export class HealthController {
   static async get(request: Request, response: Response) {
     const services: Record<string, string> = {
       database: 'OK',
       valorant: 'OK',
+      content: 'OK',
     }
 
     const database = new MySQLDatabase()
     const valorant = Valorant.create()
+    const content = new Content()
 
-    const promises = [database.ping(), valorant.ping()]
+    const promises = [database.ping(), valorant.ping(), content.ping()]
     const results = await Promise.all(promises)
 
     results.forEach(function (ping, index) {
--- src/server/infrastructure/valorant/league-repository.ts
+++ src/server/infrastructure/valorant/league-repository.ts
@@ -2,21 +2,38 @@ import { League, LeagueId, LeagueRepository, Match } from '../../../core'
 import { toLeagues } from './league-mapper'
 import { toMatches } from './match-mapper'
 import { Valorant } from '../../vendors/valorant'
+import { Content } from '../../vendors/content'
+
+const defautLeagues = [
+  '109974795266458277',
+  '106109559530232966',
+  '109974804058058602',
+]
 
 export class ValorantLeagueRepository implements LeagueRepository {
   private readonly api: Valorant
+  private readonly content: Content
 
   constructor() {
     this.api = new Valorant()
+    this.content = new Content()
   }
 
   async getAll(): Promise<League[]> {
     const leagues = await this.api.getAllLeagues()
-    return toLeagues(leagues)
+    const allowedLeagueIds = await this.getAllowedLeagues()
+    return toLeagues(leagues).filter(function (league) {
+      return allowedLeagueIds.includes(league.id)
+    })
   }
 
   async getMatches(league: LeagueId): Promise<Match[]> {
     const matches = await this.api.getMatches(league)
     return toMatches(matches)
+  }
+
+  async getAllowedLeagues() {
+    const leagueIds = await this.content.getLeagueIds()
+    return leagueIds.length > 0 ? leagueIds : defautLeagues
   }
 }
