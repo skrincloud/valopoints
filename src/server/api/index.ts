221169be77ec46a13d2913979bc77bf02b52a63d
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-04-23 21:39:24 -0500

    VLP-5 Refactor health endpoint

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,28 +1,11 @@
 import { Router } from 'express'
-import MySQLDatabase from '../vendors/mysql'
 import { LeagueController } from '../controllers/leagues'
+import { HealthController } from '../controllers/health'
 
 const router = Router()
 
 router.get('/leagues', LeagueController.getAll)
 
-router.get('/health', async function (request, response) {
-  try {
-    const database = new MySQLDatabase()
-    await database.ping()
-    response.json({
-      services: {
-        database: 'OK',
-      },
-    })
-  } catch (error) {
-    response.status(500)
-    response.json({
-      services: {
-        database: 'There is a problem in the app',
-      },
-    })
-  }
-})
+router.get('/health', HealthController.get)
 
 export default router
