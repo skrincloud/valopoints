3b8240257c28712517603aa7cc58958d88d0da16
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-04-25 18:29:17 -0500

    VLP-15 Add vitest package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  test: {
+    coverage: {
+      provider: 'c8',
+      reportsDirectory: './test/unit/coverage',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+})
