31c12a6472cfa97c9d7b259b1ddffec4960faf0b
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-04-27 14:31:04 -0500

    VLP-19 Define vitest workspace for folder

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,20 +1,26 @@
-import { defineWorkspace } from 'vitest/config'
+import { VitestEnvironment } from 'vitest'
+import { UserProjectConfigExport, defineWorkspace } from 'vitest/config'
+
+interface FolderOptions {
+  name: 'core' | 'server' | 'client'
+  environment?: VitestEnvironment
+}
+
+function defineFolder(options: FolderOptions): UserProjectConfigExport & {
+  extends: string
+} {
+  return {
+    extends: './vite.config.ts',
+    test: {
+      name: options.name,
+      include: [`src/${options.name}/**/*.test.{ts,tsx}`],
+      environment: options.environment,
+    },
+  }
+}
 
 export default defineWorkspace([
-  {
-    extends: './vite.config.ts',
-    test: {
-      name: 'client',
-      include: ['src/client/**/*.test.{ts,tsx}'],
-      environment: 'jsdom',
-    },
-  },
-  {
-    extends: './vite.config.ts',
-    test: {
-      name: 'server',
-      include: ['src/{core,server}/**/*.test.ts'],
-      environment: 'node',
-    },
-  },
+  defineFolder({ name: 'core' }),
+  defineFolder({ name: 'server' }),
+  defineFolder({ name: 'client', environment: 'jsdom' }),
 ])
