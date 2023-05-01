5602ede2ea7de673edc0dbc6c139312521f78c19
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-04-30 19:53:11 -0500

    VLP-24 Using import meta

--- src/client/components/Stage/index.tsx
+++ src/client/components/Stage/index.tsx
@@ -1,10 +1,13 @@
 import './Stage.css'
 
 function Stage() {
+  const version = APP_VERSION
+  const stage = import.meta.env.VITE_STAGE
+
   return (
     <div className="Stage">
-      <div className="Stage--small">{APP_STAGE}</div>
-      <div>v{APP_VERSION}</div>
+      <div className="Stage--small">{stage}</div>
+      <div>v{version}</div>
     </div>
   )
 }
