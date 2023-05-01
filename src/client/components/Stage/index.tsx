a58b0d7c13c2b5af95304a73d328874479ad9e6c
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-04-30 19:55:10 -0500

    VLP-24 Logs

--- src/client/components/Stage/index.tsx
+++ src/client/components/Stage/index.tsx
@@ -4,6 +4,8 @@ function Stage() {
   const version = APP_VERSION
   const stage = import.meta.env.VITE_STAGE
 
+  console.log('ENV', import.meta.env)
+
   return (
     <div className="Stage">
       <div className="Stage--small">{stage}</div>
